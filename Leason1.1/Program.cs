0aa7e0662904113f6ec47f23df956f79f15a8a47
Konstantin <kostik-kostik-12@mail.ru>
2023-04-26 22:11:41 +0300

    add Leason1.1

---
+++ Leason1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if(number < 100 && number >= 1000 )
+{
+    Console.WriteLine("Выедено не трёхзначное число");
+}
+else
+{
+int FindNumber(int x)
+{
+    x = number / 10 % 10;
+    return x;
+}
+Console.WriteLine("Вторая цифра этого числа: " + FindNumber(number));
+}
